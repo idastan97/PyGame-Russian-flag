0a34fa57ae77bdafacb59edac65e54395a83bd54
Dastan <idastan97@gmail.com>
2019-10-13 23:23:25 +0300

    blue line added

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ screen = pygame.display.set_mode(size)
 
 
 def draw():
-    line_color = pygame.Color('white')
-    line_w = 0
-    line_points = ((25, 25), (600, 100))
-    pygame.draw.rect(screen, line_color, line_points, line_w)
+    line_1_color = pygame.Color('white')
+    line_1_w = 0
+    line_1_points = ((25, 25), (600, 100))
+    pygame.draw.rect(screen, line_1_color, line_1_points, line_1_w)
+    line_2_color = pygame.Color('blue')
+    line_2_w = 0
+    line_2_points = ((25, 125), (600, 100))
+    pygame.draw.rect(screen, line_2_color, line_2_points, line_2_w)
 
 
 draw()
